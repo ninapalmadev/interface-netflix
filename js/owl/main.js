ff9589ce9df0208681c1a074861d22fcc260ff42
ninapalmadev <ninapalmadev@gmail.com>
2022-04-05 18:46:46 -0300

    creates section and footer

--- js/owl/main.js
+++ js/owl/main.js
@@ -14,3 +14,18 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+$('.owl-carousel').owlCarousel({
+    items:5,
+    loop:true,
+    margin:10,
+    merge:true,
+    responsive:{
+        678:{
+            mergeFit:true
+        },
+        1000:{
+            mergeFit:false
+        }
+    }
+});
